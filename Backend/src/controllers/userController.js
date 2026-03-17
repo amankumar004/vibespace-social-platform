901a5a3d8824e476d3eab8155dd6c1fa9f55cc76
amankumar004 <121446094+amankumar004@users.noreply.github.com>
2026-03-18 00:33:45 +0530

    feat: add update user profile functionality and enhance user route

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 const getUserProfile = async (req, res) => {
   try {
     res.status(200).json(req.user);
@@ -7,6 +9,50 @@ const getUserProfile = async (req, res) => {
   }
 };
 
+const updateUserProfile = async (req, res) => {
+  try {
+    const { username, email, bio } = req.body;
+
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (!email || !username) {
+      const existingUser = await User.findOne({
+        $or: [{ email }, { username }],
+      });
+      if (existingUser && existingUser._id.toString() !== user._id.toString()) {
+        return res
+          .status(400)
+          .json({ message: "Email or username already exists" });
+      }
+    }
+
+    user.username = username?.trim() || user.username;
+    user.email = email?.trim() || user.email;
+    user.bio = bio?.trim() || user.bio;
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "User profile updated successfully",
+      data: {
+        id: updatedUser._id,
+        username: updatedUser.username,
+        email: updatedUser.email,
+        bio: updatedUser.bio,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   getUserProfile,
+  updateUserProfile,
 };
--- Backend/src/routes/userRoute.js
+++ Backend/src/routes/userRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getUserProfile } = require("../controllers/userController");
+const {
+  getUserProfile,
+  updateUserProfile,
+} = require("../controllers/userController");
 const { protect } = require("../middlewares/authMiddleware");
 
 router.get("/me", protect, getUserProfile);
+router.put("/me", protect, updateUserProfile);
 
 module.exports = router;
